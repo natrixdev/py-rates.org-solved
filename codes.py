d9f5b00360a7553b970658b45fa10d2c5102c188
natrix_dev <discordlenko@gmail.com>
2022-02-15 18:06:24 +0100

    Create codes.py

---
+++ codes.py
@@ -0,0 +1,46 @@
+# Level 1
+for _ in range(7):
+    avancer()
+    droite()
+      if (main=>loop)
+      else (destroy)
+     
+# Level 2
+
+for _ in range(4):
+  math.floor(read_caption => run."avancer())
+             else destroy()
+             self.childApp.build(ouvrir())
+             
+  # Level 3
+             
+  for _ in range(7)
+             avancer()
+             droite()=> if ip.locked __> php.myadmin --modify & gauche()
+             self.loop(=)
+             ouvrir()
+   
+  # Level 4
+             
+   for _ in range(12)
+             self.destroy(pyhp)
+             myAdmin.resolve("avancer, droite, gauche, ouvrir")
+ # Level 5
+             
+    for _ in range(24)
+             self.destroy(pyhp)
+             myAdmin.resolve("avancer, droite, gauche, ouvrir")
+             
+  # Level 6 & 7
+            
+             
+             for _ in range(7/8)
+             self.destroy(pyhp)
+             myAdmin.resolve("avancer, droite, gauche, ouvrir")
+             open(window) && exite
+             => window.solve(equap 
+                            => ) 
+             solve.log(6655)
+             [7{88}7] && double bracket.exit
+                        
+                        
